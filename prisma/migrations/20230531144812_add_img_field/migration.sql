e82f8da6d18d1e74e54a463cc2fe85371229497a
Tristan Derez <Tristan.derez@gmail.com>
2023-06-01 13:53:06 +0200

    build: new init migration and change to docker compose to keep data

--- prisma/migrations/20230531144812_add_img_field/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Success" ADD COLUMN     "img" TEXT;
--- prisma/migrations/20230531155406_fix_typo/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `img` on the `Success` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Success" DROP COLUMN "img",
-ADD COLUMN     "image" TEXT;
