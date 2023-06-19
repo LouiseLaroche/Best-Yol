95f2db96c8b4f12e3f4388652377411f61cc5d77
Tristan Derez <Tristan.derez@gmail.com>
2023-06-19 10:33:31 +0200

    refactor: move checking

--- middlewares/authenticateToken.ts
+++ middlewares/authenticateToken.ts
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 
 export default (req: Request, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(" ")[1];
+    if (!token) return res.status(401).json({ erreur: "Accès non autorisé" });
+
     try {
-        if (!token) return res.status(401).json({ erreur: "Accès non autorisé" });
         jwt.verify(token, process.env.JWT_TOKEN as string);
         next();
     } catch (error) {
