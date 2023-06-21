fd34795b614a21d0ce7cfcddfd6ea006355394ec
Louise <llarocheprigent@gmail.com>
2023-06-21 17:17:52 +0200

    fix: no more authController on route getAllSpecies

--- routes/Species.ts
+++ routes/Species.ts
@@ -1,10 +1,9 @@
 import express, { Router } from "express";
-import authToken from "../middlewares/authToken";
 
 const router: Router = express.Router();
 
 import speciesController from "../controllers/Species"
 
-router.get("/", authToken, speciesController.getAllSpecies);
+router.get("/", speciesController.getAllSpecies);
 
 export default router;
