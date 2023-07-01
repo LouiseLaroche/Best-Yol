672a057af7280d2c6d179e55887f26d3f205198b
Tristan Derez <Tristan.derez@gmail.com>
2023-07-01 11:17:34 +0200

    build: add log on prisma client

--- utils/prismaClient.ts
+++ utils/prismaClient.ts
@@ -1,6 +1,6 @@
 import { PrismaClient } from "@prisma/client";
 
 // Create a singleton instance of the Prisma client
-const prisma = new PrismaClient();
+const prisma = new PrismaClient({ log: ["query", "info", "warn", "error"] });
 
 export default prisma;
