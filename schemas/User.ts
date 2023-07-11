e6c309c1fa3793a4b19b089b7ed45160059a8dad
Tristan Derez <Tristan.derez@gmail.com>
2023-07-11 14:18:49 +0200

    fix: add comma after error message in password validation

--- schemas/User.ts
+++ schemas/User.ts
@@ -5,7 +5,7 @@ export const SignUpSchema = z.object({
         username: z.string().min(3, { message: "Le nom d'utilisateur doit contenir au moins 3 caractères" }),
         email: z.string().email({ message: "L'email doit être un email valide" }),
         password: z.string().regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/, {
-            message: "Le mot de passe doit contenir au minimum 8 caractères un nombre et un caractère spécial",
+            message: "Le mot de passe doit contenir au minimum 8 caractères, un nombre et un caractère spécial",
         }),
     }),
 });
