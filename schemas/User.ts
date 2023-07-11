927a903a43b722fc43fa39646cbb718b07b525c4
Tristan Derez <Tristan.derez@gmail.com>
2023-07-11 14:18:07 +0200

    fix: add comma in error message from "password"

--- schemas/User.ts
+++ schemas/User.ts
@@ -14,7 +14,7 @@ export const LoginSchema = z.object({
     body: z.object({
         username: z.string().min(3, { message: "Le nom d'utilisateur doit contenir au moins 3 caractères" }),
         password: z.string().regex(/^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{8,}$/, {
-            message: "Le mot de passe doit contenir au minimum 8 caractères un nombre et un caractère spécial",
+            message: "Le mot de passe doit contenir au minimum 8 caractères, un nombre et un caractère spécial",
         }),
     }),
 });
